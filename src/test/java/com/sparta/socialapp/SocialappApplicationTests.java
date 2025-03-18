e60de1f32af053956900b6ad838836aabbc0d354
limlimjo <skyblue.777@daum.net>
2025-03-18 21:52:05 +0900

    [add] 초기 환경 세팅

---
+++ src/test/java/com/sparta/socialapp/SocialappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.socialapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
